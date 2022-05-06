82d1a455cb3b782ca6f3cae3b7c7407cd1a527f0
rakanmosa <90357793+rakanmosa@users.noreply.github.com>
2022-05-06 15:54:34 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -8,25 +8,24 @@ import numpy as np
 
 def getTrain():
     dataset=tfds.load('dtd',split="train",shuffle_files=True)
-    images=[example["image"] for example in dataset]
+    images=[tf.image.resize(example["image"], [300, 400]) for example in dataset]
     labels=[example["label"] for example in dataset]
     filenames=[example["file_name"] for example in dataset]
     return images,labels,filenames
 
 def getValidation():
     dataset=tfds.load('dtd',split="validation",shuffle_files=True)
-    images=[example["image"] for example in dataset]
+    images=[tf.image.resize(example["image"], [300, 400]) for example in dataset]
     labels=[example["label"] for example in dataset]
     filenames=[example["file_name"] for example in dataset]
     return images,labels,filenames
 
 def getTest():
     dataset=tfds.load('dtd',split="test",shuffle_files=True)
-    images=[example["image"] for example in dataset]
+    images=[tf.image.resize(example["image"], [300, 400]) for example in dataset]
     labels=[example["label"] for example in dataset]
     filenames=[example["file_name"] for example in dataset]
     return images,labels,filenames
 
 if __name__ == '__main__':
-    test=getTest()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("a")
