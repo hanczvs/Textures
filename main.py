bef2e72150064a2e04f271486824501c55335831
rakanmosa <90357793+rakanmosa@users.noreply.github.com>
2022-05-11 16:20:17 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import numpy as np
 from Dtd import Dtd2
 
 NUM_CLASSES = 47
-BATCH_SIZE = 32
+BATCH_SIZE = 1
 
 def getTrain():
     dataset=tfds.load('dtd',split="train",shuffle_files=True)
@@ -33,7 +33,7 @@ def getTest():
     return images,labels,filenames
 
 def get_files():
-    train_ds, val_ds, test_ds = tfds.load('dtd', split=['train', 'validation', 'test'], shuffle_files=True, batch_size = BATCH_SIZE)
+    train_ds, val_ds, test_ds = tfds.load('dtd', split=['train', 'validation', 'test'], shuffle_files=False, batch_size = BATCH_SIZE)
 
     train_ds = train_ds.map(lambda items: (items["image"], tf.one_hot(items["label"], NUM_CLASSES)))
 
@@ -58,54 +58,59 @@ def get_files():
 
     return train_ds, val_ds, test_ds
 
-def dtdTrain():
+def dtdTrain(n):
     dt=Dtd2()
     dm = tfds.download.DownloadManager(download_dir='DLFinal')
     #print(labels)
     datapath='DLFinal\\extracted\\TAR_GZ.robot.ox.ac.uk_vgg_dtd_downl_dtd-r1.0.15ChVpSpJUKO1lhKDRgKqJTkUdVyVsM_56tbQc5X44gU.tar.gz'
     images, labels, filenames=[],[],[]
-    for i in range(1,11):
+    for i in range(1,n+1):
         x= dt._generate_examples(datapath,'train'+str(i))
-        labels=labels+[example[1]["label"] for example in x]
+        labels=labels+[tf.keras.preprocessing.text.one_hot((example[1]["label"]),47) for example in x]
         x= dt._generate_examples(datapath,'train'+str(i))
         images=images+[tf.image.resize(tf.image.decode_image(tf.io.read_file(example[1]["image"])), [224, 224])/ 255.0 for example in x]
         x= dt._generate_examples(datapath,'train'+str(i))
         filenames=filenames+[example[1]["file_name"] for example in x]
         print(i)
-    return images,labels,filenames
+    label = [tf.one_hot(i[0],47) for i in labels]
+    return images,label,filenames
+def dtdTest(n):
+    dt=Dtd2()
+    dm = tfds.download.DownloadManager(download_dir='DLFinal')
+    #print(labels)
+    datapath='DLFinal\\extracted\\TAR_GZ.robot.ox.ac.uk_vgg_dtd_downl_dtd-r1.0.15ChVpSpJUKO1lhKDRgKqJTkUdVyVsM_56tbQc5X44gU.tar.gz'
+    images, labels, filenames=[],[],[]
+    for i in range(1,n+1):
+        x= dt._generate_examples(datapath,'test'+str(i))
+        labels=labels+[tf.keras.preprocessing.text.one_hot((example[1]["label"]),47) for example in x]
+        x= dt._generate_examples(datapath,'test'+str(i))
+        images=images+[tf.image.resize(tf.image.decode_image(tf.io.read_file(example[1]["image"])), [224, 224])/ 255.0 for example in x]
+        x= dt._generate_examples(datapath,'test'+str(i))
+        filenames=filenames+[example[1]["file_name"] for example in x]
+        print(i)
+    label = [tf.one_hot(i[0],47) for i in labels]
+    return images,label,filenames
 
-def dtdTest():
+def dtdValidation(n):
     dt=Dtd2()
     dm = tfds.download.DownloadManager(download_dir='DLFinal')
     #print(labels)
     datapath='DLFinal\\extracted\\TAR_GZ.robot.ox.ac.uk_vgg_dtd_downl_dtd-r1.0.15ChVpSpJUKO1lhKDRgKqJTkUdVyVsM_56tbQc5X44gU.tar.gz'
     images, labels, filenames=[],[],[]
-    for i in range(1,11):
-        x= dt._generate_examples(datapath,'test'+str(i))
-        labels=labels+[example[1]["label"] for example in x]
-        x= dt._generate_examples(datapath,'test'+str(i))
+    for i in range(1,n+1):
+        x= dt._generate_examples(datapath,'val'+str(i))
+        labels=labels+[tf.keras.preprocessing.text.one_hot((example[1]["label"]),47) for example in x]
+        x= dt._generate_examples(datapath,'val'+str(i))
         images=images+[tf.image.resize(tf.image.decode_image(tf.io.read_file(example[1]["image"])), [224, 224])/ 255.0 for example in x]
-        x= dt._generate_examples(datapath,'test'+str(i))
+        x= dt._generate_examples(datapath,'val'+str(i))
         filenames=filenames+[example[1]["file_name"] for example in x]
         print(i)
-    return images,labels,filenames
-
-def dtdValidation():
-    dt=Dtd2()
-    dm = tfds.download.DownloadManager(download_dir='DLFinal')
-    #print(labels)
-    datapath='DLFinal\\extracted\\TAR_GZ.robot.ox.ac.uk_vgg_dtd_downl_dtd-r1.0.15ChVpSpJUKO1lhKDRgKqJTkUdVyVsM_56tbQc5X44gU.tar.gz'
-    images, labels, filenames=[],[],[]
-    for i in range(1,11):
-        x= dt._generate_examples(datapath,'validation'+str(i))
-        labels=labels+[example[1]["label"] for example in x]
-        x= dt._generate_examples(datapath,'validation'+str(i))
-        images=images+[tf.image.resize(tf.image.decode_image(tf.io.read_file(example[1]["image"])), [224, 224])/ 255.0 for example in x]
-        x= dt._generate_examples(datapath,'validation'+str(i))
-        filenames=filenames+[example[1]["file_name"] for example in x]
-        print(i)
-    return images,labels,filenames
+    label = [tf.one_hot(i[0],47) for i in labels]
+    return images,label,filenames
 
 if __name__ == "__main__":
-    images,labels,filenames=dtd()
+    images,labels,filenames=dtdTrain()
+    images,labels,filenames=dtdTest()
+    images,labels,filenames=dtdValidation()
+
     print(len(images))
